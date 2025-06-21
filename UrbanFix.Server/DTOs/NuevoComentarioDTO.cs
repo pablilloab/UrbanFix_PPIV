8a4a754841c58ef614158e0ccc4fba70d06c7e31
Javier Dojas <dojas.javier@gmail.com>
2025-06-21 14:21:03 -0300

    Dto nuevo comentario

---
+++ UrbanFix.Server/DTOs/NuevoComentarioDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace UrbanFix.Server.Models.DTOs
+{
+    public class NuevoComentarioDto
+    {
+        public string Contenido { get; set; }
+        public int UsuarioId { get; set; }
+        public int ReporteId { get; set; }
+    }
+}
+
