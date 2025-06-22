fd59a3aa877c0877fda5b27b9e7c5ea332112e9a
Javier Dojas <dojas.javier@gmail.com>
2025-06-22 13:44:02 -0300

    Agregadas entidades de las bases de datos

---
+++ UrbanFix.Server/Models/Comentario.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UrbanFix.Server.Models
+{
+    public class Comentario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Contenido { get; set; } = string.Empty;
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        // Relación con Reporte
+        [ForeignKey("Reporte")]
+        public int ReporteId { get; set; }
+        public Reporte? Reporte { get; set; }
+
+        // Relación con Usuario
+        [ForeignKey("Usuario")]
+        public int UsuarioId { get; set; }
+        public Usuario? Usuario { get; set; }
+    }
+}
+
